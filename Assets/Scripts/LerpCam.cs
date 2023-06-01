98d47f0158e71062893066eda1c6a17bd6793408
wmcentire <wmcentire@student.neumont.edu>
2023-06-01 11:35:49 -0600

    copied lerp cam

--- Assets/Scripts/LerpCam.cs
+++ Assets/Scripts/LerpCam.cs
@@ -12,7 +12,15 @@ public class LerpCam : MonoBehaviour
     {
         if (target != null)
         {
+//<<<<<<< HEAD
             transform.position = Vector2.Lerp(transform.position, target.position, ratio);
+//=======
+            var temp = Vector3.Lerp(transform.position, target.position, ratio);
+
+            Vector3 halfway = new Vector3(temp.x, temp.y, transform.position.z);
+
+            transform.position = halfway;
+//>>>>>>> 89e8406a875b73897f11a5e298ffdbe8915430ed
         }
     }
 }
